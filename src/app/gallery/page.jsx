fcc52962d7e38b38e7b73df92a4c7c5684543bd2
thegrimkiller22 <anubhav44r@gmail.com>
2024-12-31 16:58:41 +0530

    photo add

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -3,15 +3,7 @@ import Link from "next/link";
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
 
-const data = {
-  "event":[
-    "1.jpg",
-    "2.jpg",
-    "3.jpg",
-    "4.jpg",
-    "5.jpg",
-  ],
-}
+
 
 const galleryItems = [
   {
